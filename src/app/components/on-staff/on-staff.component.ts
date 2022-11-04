192fb78ce87e317caa6ef2ba2cc647823781e652
Ethan Cahill <andersmith29@gmail.com>
2022-11-04 16:01:49 -0400

    completed framwork of staff section, still want to create code for dynamically generating staff icons/avatars

--- src/app/components/on-staff/on-staff.component.ts
+++ src/app/components/on-staff/on-staff.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-on-staff',
   templateUrl: './on-staff.component.html',
   styleUrls: ['./on-staff.component.css']
 })
 export class OnStaffComponent implements OnInit {
+
+check = faCheck
 
   constructor() { }
 
